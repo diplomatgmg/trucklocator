a066d735cd13c1fa8cde50cf98e7e33bc57f6d77
Кирилл Попов <diplomatgmg@gmail.com>
2023-05-24 18:45:48 +0300

    Автоматическая загрузка Локаций при миграции

---
+++ apps/main/signals.py
@@ -0,0 +1,28 @@
+import csv
+
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+
+from apps.main.models import Location
+
+
+@receiver(post_migrate)
+def load_locations(sender, **kwargs):
+    if sender.name == "apps.main":
+        with open("uszips.csv", "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                city = row["city"]
+                state = row["state_name"]
+                zip_code = row["zip"]
+                latitude = row["lat"]
+                longitude = row["lng"]
+
+                Location.objects.create(
+                    city=city,
+                    state=state,
+                    zip_code=zip_code,
+                    latitude=latitude,
+                    longitude=longitude,
+                )
+        print("locations successfully loaded")
